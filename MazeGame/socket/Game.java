c283e6f98d9c746e30228b7f369de1f3ec9bf369
e0046714 <e0046714@SOCTF-PL6-014.comp.nus.edu.sg>
2016-09-11 13:32:37 +0800

    adadsa

--- MazeGame/socket/Game.java
+++ MazeGame/socket/Game.java
@@ -75,6 +75,10 @@ public class Game implements ServerEventListener {
 			
 			// After the primary and backup server is up, create the local Game
 			try {
+				Connect2Server c2s = new Connect2Server(playerList, this.localPlayer);
+				c2s.start();
+				
+				
 				GameThread gameThread = new GameThread(n,k, this.localPlayer, playerList);
 				gameThread.start();
 			} catch (Exception e) {
@@ -106,3 +110,83 @@ public class Game implements ServerEventListener {
 	}
 }
 
+
+class Connect2Server extends Thread{
+	BufferedReader inFromServer;
+	private Player localPlayer;
+	private Player[] playerList;
+	private Socket conn2Server;
+	
+	public Connect2Server(Player[] playerList, Player localPlayer){
+		this.localPlayer = localPlayer;
+		this.playerList = playerList;
+	}
+	
+	public void run(){
+		while(true){
+			try {
+				// find the actual primary server
+				Player primaryServer = null;
+				int primaryServeIndex = 0;
+				for(int i=0; i<playerList.length; i++){
+					primaryServer = playerList[i];
+					try {
+						System.out.println("LocalPlayer try to connect to " + primaryServer.getName() + " " + primaryServer.getIp() + " " + primaryServer.getPort());
+						this.conn2Server = new Socket(primaryServer.getIp(), primaryServer.getPort());
+						System.out.println("LocalPlayer find primary server: " + primaryServer.getName() + " " + primaryServer.getIp() + " " + primaryServer.getPort());
+						primaryServeIndex = i;
+						break;
+					} catch (Exception e) {
+						ServerThread.removePlayer(primaryServer);
+						System.out.println("Connect to " + primaryServer.getName() + " failed! Try next one!");
+					}
+				}
+
+				
+				// Send local player to server
+				DataOutputStream out = new DataOutputStream(this.conn2Server.getOutputStream());
+		        out.writeBytes(this.localPlayer.toStr() + "\n");
+		        out.flush();
+		        
+		        inFromServer = new BufferedReader(new InputStreamReader(this.conn2Server.getInputStream()));
+		        try{
+			        while(true){
+			        	// receive the full list of players
+			        	String msg = inFromServer.readLine();
+			        	String[] msgToken = msg.split(";");
+			        	
+			        	if (msgToken[0].equals("BK")){
+			        		System.out.println("BackupServer received backup info: " + msg);
+//			        		if (msgToken[1].equals("IF")){
+//			        			for(int i=2; i<msgToken.length; i++){
+//			        				Player tmp = new Player(msgToken[i]);
+//			        				if(ServerThread.addNewPlayer(tmp)){
+//			        					System.out.println("==>Player "+ msgToken[i] +" added in list");
+//			        				}
+//			        				else
+//			        					System.out.println("Player "+ msgToken[i] +" already in list");
+//			        			}
+//			        		}
+			        		// if = MZ then back up maze info
+			        	}
+			        	else if (msgToken[0].equals("IF")){
+			        		System.out.println("LocalPlayer receive full list of current players: " + msg);
+			        	}
+			        	else if (msgToken[0] == "MZ"){
+			        		System.out.println("LocalPlayer receive maze info: " + msg);
+			        	}
+			        	else
+			    			System.out.println("Unkown message received: " + msg);
+			        }
+		        } catch (IOException e) {
+					// TODO Auto-generated catch block
+		        	ServerThread.removePlayer(primaryServer);
+					System.out.println("Primary Server down!");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
